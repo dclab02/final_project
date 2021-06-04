4cddbacbba5cfc69dbc8b9ea4a22766cece60582
orange hsu <orange21201@gmail.com>
2021-06-04 22:35:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# DCLab final project - SDR demod equilizer
+2021 Spring, NTUEE Digital Circuit Lab, team02  
+
+
---
+++ final/final_inst.vhd
@@ -0,0 +1,13 @@
+	component final is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component final;
+
+	u0 : component final
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
