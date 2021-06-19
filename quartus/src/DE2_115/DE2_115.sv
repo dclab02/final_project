81c96ff5a32286cf48fd343ad1ffae612dcf39f3
orange hsu <orange21201@gmail.com>
2021-06-20 02:03:04 +0800

    0620 update

---
+++ quartus/src/DE2_115/DE2_115.sv
@@ -0,0 +1,378 @@
+module DE2_115 (
+	input CLOCK_50,
+	input CLOCK2_50,
+	input CLOCK3_50,
+	input ENETCLK_25,
+	input SMA_CLKIN,
+	output SMA_CLKOUT,
+	output [8:0] LEDG,
+	output [17:0] LEDR,
+	input [3:0] KEY,
+	input [17:0] SW,
+	output [6:0] HEX0,
+	output [6:0] HEX1,
+	output [6:0] HEX2,
+	output [6:0] HEX3,
+	output [6:0] HEX4,
+	output [6:0] HEX5,
+	output [6:0] HEX6,
+	output [6:0] HEX7,
+	output LCD_BLON,
+	inout [7:0] LCD_DATA,
+	output LCD_EN,
+	output LCD_ON,
+	output LCD_RS,
+	output LCD_RW,
+	output UART_CTS,
+	input UART_RTS,
+	input UART_RXD,
+	output UART_TXD,
+	inout PS2_CLK,
+	inout PS2_DAT,
+	inout PS2_CLK2,
+	inout PS2_DAT2,
+	output SD_CLK,
+	inout SD_CMD,
+	inout [3:0] SD_DAT,
+	input SD_WP_N,
+	output [7:0] VGA_B,
+	output VGA_BLANK_N,
+	output VGA_CLK,
+	output [7:0] VGA_G,
+	output VGA_HS,
+	output [7:0] VGA_R,
+	output VGA_SYNC_N,
+	output VGA_VS,
+	input AUD_ADCDAT,
+	inout AUD_ADCLRCK,
+	inout AUD_BCLK,
+	output AUD_DACDAT,
+	inout AUD_DACLRCK,
+	output AUD_XCK,
+	output EEP_I2C_SCLK,
+	inout EEP_I2C_SDAT,
+	output I2C_SCLK,
+	inout I2C_SDAT,
+	output ENET0_GTX_CLK,
+	input ENET0_INT_N,
+	output ENET0_MDC,
+	input ENET0_MDIO,
+	output ENET0_RST_N,
+	input ENET0_RX_CLK,
+	input ENET0_RX_COL,
+	input ENET0_RX_CRS,
+	input [3:0] ENET0_RX_DATA,
+	input ENET0_RX_DV,
+	input ENET0_RX_ER,
+	input ENET0_TX_CLK,
+	output [3:0] ENET0_TX_DATA,
+	output ENET0_TX_EN,
+	output ENET0_TX_ER,
+	input ENET0_LINK100,
+	output ENET1_GTX_CLK,
+	input ENET1_INT_N,
+	output ENET1_MDC,
+	input ENET1_MDIO,
+	output ENET1_RST_N,
+	input ENET1_RX_CLK,
+	input ENET1_RX_COL,
+	input ENET1_RX_CRS,
+	input [3:0] ENET1_RX_DATA,
+	input ENET1_RX_DV,
+	input ENET1_RX_ER,
+	input ENET1_TX_CLK,
+	output [3:0] ENET1_TX_DATA,
+	output ENET1_TX_EN,
+	output ENET1_TX_ER,
+	input ENET1_LINK100,
+	input TD_CLK27,
+	input [7:0] TD_DATA,
+	input TD_HS,
+	output TD_RESET_N,
+	input TD_VS,
+	inout [15:0] OTG_DATA,
+	output [1:0] OTG_ADDR,
+	output OTG_CS_N,
+	output OTG_WR_N,
+	output OTG_RD_N,
+	input OTG_INT,
+	output OTG_RST_N,
+	input IRDA_RXD,
+	output [12:0] DRAM_ADDR,
+	output [1:0] DRAM_BA,
+	output DRAM_CAS_N,
+	output DRAM_CKE,
+	output DRAM_CLK,
+	output DRAM_CS_N,
+	inout [31:0] DRAM_DQ,
+	output [3:0] DRAM_DQM,
+	output DRAM_RAS_N,
+	output DRAM_WE_N,
+	output [19:0] SRAM_ADDR,
+	output SRAM_CE_N,
+	inout [15:0] SRAM_DQ,
+	output SRAM_LB_N,
+	output SRAM_OE_N,
+	output SRAM_UB_N,
+	output SRAM_WE_N,
+	output [22:0] FL_ADDR,
+	output FL_CE_N,
+	inout [7:0] FL_DQ,
+	output FL_OE_N,
+	output FL_RST_N,
+	input FL_RY,
+	output FL_WE_N,
+	output FL_WP_N,
+	inout [35:0] GPIO,
+	input HSMC_CLKIN_P1,
+	input HSMC_CLKIN_P2,
+	input HSMC_CLKIN0,
+	output HSMC_CLKOUT_P1,
+	output HSMC_CLKOUT_P2,
+	output HSMC_CLKOUT0,
+	inout [3:0] HSMC_D,
+	input [16:0] HSMC_RX_D_P,
+	output [16:0] HSMC_TX_D_P,
+	inout [6:0] EX_IO
+);
+
+logic key0down, key1down, key2down, key3down;
+// logic CLK_12M, CLK_100K, CLK_800K;
+
+// assign AUD_XCK = CLK_12M;
+
+// Altpll pll0( // generate with qsys, please follow lab2 tutorials
+// 	.clk_clk(CLOCK_50),
+// 	.reset_reset_n(key3down),
+// 	.altpll_12m_clk(CLK_12M),
+// 	.altpll_100k_clk(CLK_100K),
+// 	.altpll_800k_clk(CLK_800K)
+// );
+
+logic clk_int;
+logic rst_int;
+
+logic pll_locked;
+
+logic clk90_int;
+
+altpll #(
+    .bandwidth_type("AUTO"),
+    .clk0_divide_by(2),
+    .clk0_duty_cycle(50),
+    .clk0_multiply_by(5),
+    .clk0_phase_shift("0"),
+    .clk1_divide_by(2),
+    .clk1_duty_cycle(50),
+    .clk1_multiply_by(5),
+    .clk1_phase_shift("2000"),
+    .compensate_clock("CLK0"),
+    .inclk0_input_frequency(20000),
+    .intended_device_family("Cyclone IV E"),
+    .operation_mode("NORMAL"),
+    .pll_type("AUTO"),
+    .port_activeclock("PORT_UNUSED"),
+    .port_areset("PORT_USED"),
+    .port_clkbad0("PORT_UNUSED"),
+    .port_clkbad1("PORT_UNUSED"),
+    .port_clkloss("PORT_UNUSED"),
+    .port_clkswitch("PORT_UNUSED"),
+    .port_configupdate("PORT_UNUSED"),
+    .port_fbin("PORT_UNUSED"),
+    .port_inclk0("PORT_USED"),
+    .port_inclk1("PORT_UNUSED"),
+    .port_locked("PORT_USED"),
+    .port_pfdena("PORT_UNUSED"),
+    .port_phasecounterselect("PORT_UNUSED"),
+    .port_phasedone("PORT_UNUSED"),
+    .port_phasestep("PORT_UNUSED"),
+    .port_phaseupdown("PORT_UNUSED"),
+    .port_pllena("PORT_UNUSED"),
+    .port_scanaclr("PORT_UNUSED"),
+    .port_scanclk("PORT_UNUSED"),
+    .port_scanclkena("PORT_UNUSED"),
+    .port_scandata("PORT_UNUSED"),
+    .port_scandataout("PORT_UNUSED"),
+    .port_scandone("PORT_UNUSED"),
+    .port_scanread("PORT_UNUSED"),
+    .port_scanwrite("PORT_UNUSED"),
+    .port_clk0("PORT_USED"),
+    .port_clk1("PORT_USED"),
+    .port_clk2("PORT_UNUSED"),
+    .port_clk3("PORT_UNUSED"),
+    .port_clk4("PORT_UNUSED"),
+    .port_clk5("PORT_UNUSED"),
+    .port_clkena0("PORT_UNUSED"),
+    .port_clkena1("PORT_UNUSED"),
+    .port_clkena2("PORT_UNUSED"),
+    .port_clkena3("PORT_UNUSED"),
+    .port_clkena4("PORT_UNUSED"),
+    .port_clkena5("PORT_UNUSED"),
+    .port_extclk0("PORT_UNUSED"),
+    .port_extclk1("PORT_UNUSED"),
+    .port_extclk2("PORT_UNUSED"),
+    .port_extclk3("PORT_UNUSED"),
+    .self_reset_on_loss_lock("ON"),
+    .width_clock(5)
+)
+altpll_component (
+    .areset(~KEY[3]),
+    .inclk({1'b0, CLOCK_50}),
+    .clk({clk90_int, clk_int}),
+    .locked(pll_locked),
+    .activeclock(),
+    .clkbad(),
+    .clkena({6{1'b1}}),
+    .clkloss(),
+    .clkswitch(1'b0),
+    .configupdate(1'b0),
+    .enable0(),
+    .enable1(),
+    .extclk(),
+    .extclkena({4{1'b1}}),
+    .fbin(1'b1),
+    .fbmimicbidir(),
+    .fbout(),
+    .fref(),
+    .icdrclk(),
+    .pfdena(1'b1),
+    .phasecounterselect({4{1'b1}}),
+    .phasedone(),
+    .phasestep(1'b1),
+    .phaseupdown(1'b1),
+    .pllena(1'b1),
+    .scanaclr(1'b0),
+    .scanclk(1'b0),
+    .scanclkena(1'b1),
+    .scandata(1'b0),
+    .scandataout(),
+    .scandone(),
+    .scanread(1'b0),
+    .scanwrite(1'b0),
+    .sclkout0(),
+    .sclkout1(),
+    .vcooverrange(),
+    .vcounderrange()
+);
+
+// // qsys for uart communication
+// Final_qsys my_qsys(
+// 	.clk_clk(CLOCK_50),
+// 	.rst_reset_n(KEY[0]),
+// 	.uart_0_external_connection_rxd(UART_RXD),
+// 	.uart_0_external_connection_txd(UART_TXD)
+// );
+
+
+// you can decide key down settings on your own, below is just an example
+Debounce deb0(
+	.i_in(KEY[0]), // Record/Pause
+	.i_rst_n(KEY[3]),
+	.i_clk(CLOCK_50),
+	.o_neg(key0down) 
+);
+
+Debounce deb1(
+	.i_in(KEY[1]), // Play/Pause
+	.i_rst_n(KEY[3]),
+	.i_clk(CLOCK_50),
+	.o_neg(key1down) 
+);
+
+Debounce deb2(
+	.i_in(KEY[2]), // Stop
+	.i_rst_n(KEY[3]),
+	.i_clk(CLOCK_50),
+	.o_neg(key2down) 
+);
+
+final_project_core top0(
+	.i_rst_n(KEY[3]),
+	.i_clk(CLK_12M),
+	.i_key_0(key0down),
+	.i_key_1(key1down),
+	.i_key_2(key2down),
+	// .i_speed(SW[3:0]), // design how user can decide mode on your own
+	
+	// SRAM
+	.o_SRAM_ADDR(SRAM_ADDR), // [19:0]
+	.io_SRAM_DQ(SRAM_DQ),    // [15:0]
+	.o_SRAM_WE_N(SRAM_WE_N),
+	.o_SRAM_CE_N(SRAM_CE_N),
+	.o_SRAM_OE_N(SRAM_OE_N),
+	.o_SRAM_LB_N(SRAM_LB_N),
+	.o_SRAM_UB_N(SRAM_UB_N),
+	
+	// I2C
+	// .i_clk_100k(CLK_100K),
+	// .o_I2C_SCLK(I2C_SCLK),
+	// .io_I2C_SDAT(I2C_SDAT),
+	
+	// AudPlayer
+	// .i_AUD_ADCDAT(AUD_ADCDAT),
+	// .i_AUD_ADCLRCK(AUD_ADCLRCK),
+	// .i_AUD_BCLK(AUD_BCLK),
+	// .i_AUD_DACLRCK(AUD_DACLRCK),
+	// .o_AUD_DACDAT(AUD_DACDAT)
+
+	// SEVENDECODER (optional display)
+	// .o_record_time(recd_time),
+	// .o_play_time(play_time),
+
+	// LCD (optional display)
+	// .i_clk_800k(CLK_800K),
+	// .o_LCD_DATA(LCD_DATA), // [7:0]
+	// .o_LCD_EN(LCD_EN),
+	// .o_LCD_RS(LCD_RS),
+	// .o_LCD_RW(LCD_RW),
+	// .o_LCD_ON(LCD_ON),
+	// .o_LCD_BLON(LCD_BLON),
+
+	// LED
+	// .o_ledg(LEDG), // [8:0]
+	// .o_ledr(LEDR) // [17:0]
+    /*
+     * Clock: 125MHz
+     * Synchronous reset
+     */
+
+    /*
+     * GPIO
+     */
+    // .btn(btn_int),
+    // .sw(sw_int),
+    .led_g(LEDG),
+    // .ledr(LEDR),
+    .hex0(HEX0),
+    .hex1(HEX1),
+    // .hex2(HEX2),
+    // .hex3(HEX3),
+    // .hex4(HEX4),
+    // .hex5(HEX5),
+    // .hex6(HEX6),
+    // .hex7(HEX7),
+    // .gpio(GPIO),
+
+    /*
+     * Ethernet: 1000BASE-T RGMII
+     */
+    .phy0_rx_clk(ENET0_RX_CLK),
+    .phy0_rxd(ENET0_RX_DATA),
+    .phy0_rx_ctl(ENET0_RX_DV),
+    .phy0_tx_clk(ENET0_GTX_CLK),
+    .phy0_txd(ENET0_TX_DATA),
+    .phy0_tx_ctl(ENET0_TX_EN),
+    .phy0_reset_n(ENET0_RST_N),
+    .phy0_int_n(ENET0_INT_N)
+
+    // .phy1_rx_clk(ENET1_RX_CLK),
+    // .phy1_rxd(ENET1_RX_DATA),
+    // .phy1_rx_ctl(ENET1_RX_DV),
+    // .phy1_tx_clk(ENET1_GTX_CLK),
+    // .phy1_txd(ENET1_TX_DATA),
+    // .phy1_tx_ctl(ENET1_TX_EN),
+    // .phy1_reset_n(ENET1_RST_N),
+    // .phy1_int_n(ENET1_INT_N)
+);
+
+endmodule
---
+++ quartus/src/final_project_core.sv
@@ -0,0 +1,535 @@
+module final_project_core (
+    input i_rst_n,
+	input i_clk,
+	input i_clk90,
+
+    input i_key_0,
+	input i_key_1,
+	input i_key_2,
+
+    // SRAM
+	output [19:0] o_SRAM_ADDR,
+	inout  [15:0] io_SRAM_DQ,
+	output        o_SRAM_WE_N,
+	output        o_SRAM_CE_N,
+	output        o_SRAM_OE_N,
+	output        o_SRAM_LB_N,
+	output        o_SRAM_UB_N,
+
+	/*
+     * Ethernet: 1000BASE-T RGMII
+     */
+    input        phy0_rx_clk,
+    input  [3:0] phy0_rxd,
+    input        phy0_rx_ctl,
+    output       phy0_tx_clk,
+    output [3:0] phy0_txd,
+    output       phy0_tx_ctl,
+    output       phy0_reset_n,
+    input        phy0_int_n,
+
+    output [8:0] led_g,
+    // .ledr(LEDR),
+    output [6:0] hex0,
+    output [6:0] hex1
+);
+
+// Ethernet Configuration
+logic [47:0] local_mac   = 48'h02_00_00_00_00_00;
+logic [31:0] local_ip    = {8'd192, 8'd168, 8'd50,   8'd168};
+logic [31:0] gateway_ip  = {8'd192, 8'd168, 8'd50,   8'd1};
+logic [31:0] subnet_mask = {8'd255, 8'd255, 8'd255, 8'd0};
+
+// AXI between MAC and Ethernet modules
+logic [7:0] rx_axis_tdata;
+logic rx_axis_tvalid;
+logic rx_axis_tready;
+logic rx_axis_tlast;
+logic rx_axis_tuser;
+
+logic [7:0] tx_axis_tdata;
+logic tx_axis_tvalid;
+logic tx_axis_tready;
+logic tx_axis_tlast;
+logic tx_axis_tuser;
+
+// Ethernet frame between Ethernet modules and UDP stack
+logic rx_eth_hdr_ready;
+logic rx_eth_hdr_valid;
+logic [47:0] rx_eth_dest_mac;
+logic [47:0] rx_eth_src_mac;
+logic [15:0] rx_eth_type;
+logic [7:0] rx_eth_payload_axis_tdata;
+logic rx_eth_payload_axis_tvalid;
+logic rx_eth_payload_axis_tready;
+logic rx_eth_payload_axis_tlast;
+logic rx_eth_payload_axis_tuser;
+
+logic tx_eth_hdr_ready;
+logic tx_eth_hdr_valid;
+logic [47:0] tx_eth_dest_mac;
+logic [47:0] tx_eth_src_mac;
+logic [15:0] tx_eth_type;
+logic [7:0] tx_eth_payload_axis_tdata;
+logic tx_eth_payload_axis_tvalid;
+logic tx_eth_payload_axis_tready;
+logic tx_eth_payload_axis_tlast;
+logic tx_eth_payload_axis_tuser;
+
+// IP frame connections
+logic rx_ip_hdr_valid;
+logic rx_ip_hdr_ready;
+logic [47:0] rx_ip_eth_dest_mac;
+logic [47:0] rx_ip_eth_src_mac;
+logic [15:0] rx_ip_eth_type;
+logic [3:0] rx_ip_version;
+logic [3:0] rx_ip_ihl;
+logic [5:0] rx_ip_dscp;
+logic [1:0] rx_ip_ecn;
+logic [15:0] rx_ip_length;
+logic [15:0] rx_ip_identification;
+logic [2:0] rx_ip_flags;
+logic [12:0] rx_ip_fragment_offset;
+logic [7:0] rx_ip_ttl;
+logic [7:0] rx_ip_protocol;
+logic [15:0] rx_ip_header_checksum;
+logic [31:0] rx_ip_source_ip;
+logic [31:0] rx_ip_dest_ip;
+logic [7:0] rx_ip_payload_axis_tdata;
+logic rx_ip_payload_axis_tvalid;
+logic rx_ip_payload_axis_tready;
+logic rx_ip_payload_axis_tlast;
+logic rx_ip_payload_axis_tuser;
+
+logic tx_ip_hdr_valid;
+logic tx_ip_hdr_ready;
+logic [5:0] tx_ip_dscp;
+logic [1:0] tx_ip_ecn;
+logic [15:0] tx_ip_length;
+logic [7:0] tx_ip_ttl;
+logic [7:0] tx_ip_protocol;
+logic [31:0] tx_ip_source_ip;
+logic [31:0] tx_ip_dest_ip;
+logic [7:0] tx_ip_payload_axis_tdata;
+logic tx_ip_payload_axis_tvalid;
+logic tx_ip_payload_axis_tready;
+logic tx_ip_payload_axis_tlast;
+logic tx_ip_payload_axis_tuser;
+
+// UDP frame connections
+logic rx_udp_hdr_valid;
+logic rx_udp_hdr_ready;
+logic [47:0] rx_udp_eth_dest_mac;
+logic [47:0] rx_udp_eth_src_mac;
+logic [15:0] rx_udp_eth_type;
+logic [3:0] rx_udp_ip_version;
+logic [3:0] rx_udp_ip_ihl;
+logic [5:0] rx_udp_ip_dscp;
+logic [1:0] rx_udp_ip_ecn;
+logic [15:0] rx_udp_ip_length;
+logic [15:0] rx_udp_ip_identification;
+logic [2:0] rx_udp_ip_flags;
+logic [12:0] rx_udp_ip_fragment_offset;
+logic [7:0] rx_udp_ip_ttl;
+logic [7:0] rx_udp_ip_protocol;
+logic [15:0] rx_udp_ip_header_checksum;
+logic [31:0] rx_udp_ip_source_ip;
+logic [31:0] rx_udp_ip_dest_ip;
+logic [15:0] rx_udp_source_port;
+logic [15:0] rx_udp_dest_port;
+logic [15:0] rx_udp_length;
+logic [15:0] rx_udp_checksum;
+logic [7:0] rx_udp_payload_axis_tdata;
+logic rx_udp_payload_axis_tvalid;
+logic rx_udp_payload_axis_tready;
+logic rx_udp_payload_axis_tlast;
+logic rx_udp_payload_axis_tuser;
+
+logic tx_udp_hdr_valid;
+logic tx_udp_hdr_ready;
+logic [5:0] tx_udp_ip_dscp;
+logic [1:0] tx_udp_ip_ecn;
+logic [7:0] tx_udp_ip_ttl;
+logic [31:0] tx_udp_ip_source_ip;
+logic [31:0] tx_udp_ip_dest_ip;
+logic [15:0] tx_udp_source_port;
+logic [15:0] tx_udp_dest_port;
+logic [15:0] tx_udp_length;
+logic [15:0] tx_udp_checksum;
+logic [7:0] tx_udp_payload_axis_tdata;
+logic tx_udp_payload_axis_tvalid;
+logic tx_udp_payload_axis_tready;
+logic tx_udp_payload_axis_tlast;
+logic tx_udp_payload_axis_tuser;
+
+logic [7:0] rx_fifo_udp_payload_axis_tdata;
+logic rx_fifo_udp_payload_axis_tvalid;
+logic rx_fifo_udp_payload_axis_tready;
+logic rx_fifo_udp_payload_axis_tlast;
+logic rx_fifo_udp_payload_axis_tuser;
+
+logic [7:0] tx_fifo_udp_payload_axis_tdata;
+logic tx_fifo_udp_payload_axis_tvalid;
+logic tx_fifo_udp_payload_axis_tready;
+logic tx_fifo_udp_payload_axis_tlast;
+logic tx_fifo_udp_payload_axis_tuser;
+
+logic [2:0] state_r, state_w;
+
+logic [19:0] addr_record;
+logic [19:0] addr_read;
+
+logic match_cond;
+// logic no_match = !match_cond;
+logic match_cond_reg, no_match_reg;
+
+logic [7:0] led_reg = 0;
+logic [15:0] data_record;
+
+// assign o_SRAM_ADDR = (state_r == S_RECD) ? addr_record : addr_read[19:0];
+// assign io_SRAM_DQ  = (state_r == S_RECD) ? data_record : 16'dz; // sram_dq as output
+// assign play_data   = (state_r != S_RECD) ? io_SRAM_DQ : 16'd0; // sram_dq as input
+
+// assign o_SRAM_WE_N = (state_r == S_RECD) ? 1'b0 : 1'b1;
+assign o_SRAM_CE_N = 1'b0;
+assign o_SRAM_OE_N = 1'b0;
+assign o_SRAM_LB_N = 1'b0;
+assign o_SRAM_UB_N = 1'b0;
+
+localparam S_IDLE = 3'd0;
+localparam S_TMP  = 3'd1;
+localparam S_RECD = 3'd3;
+
+always_comb begin
+    state_w = state_r;
+    match_cond = (rx_udp_dest_port == 1234);
+
+    case (state_r)
+        S_IDLE: begin
+            if (rx_udp_payload_axis_tvalid) begin
+            // if udp data available, send to uart
+                state_w = S_TMP;
+
+		    end
+            
+        end
+        S_TMP: begin
+            led_reg = tx_udp_payload_axis_tdata;
+            
+        end
+        default: begin
+            state_w = state_r;
+        end
+    endcase
+end
+
+always_ff @( posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+        state_r <= S_IDLE;
+
+    end
+    else begin
+        state_r <= state_w;
+
+
+        
+    end
+    
+end
+
+assign tx_udp_hdr_valid = rx_udp_hdr_valid && match_cond;
+assign rx_udp_hdr_ready = (tx_eth_hdr_ready && match_cond) || !match_cond;
+assign tx_udp_ip_dscp = 0;
+assign tx_udp_ip_ecn = 0;
+assign tx_udp_ip_ttl = 64;
+assign tx_udp_ip_source_ip = local_ip;
+assign tx_udp_ip_dest_ip = rx_udp_ip_source_ip;
+assign tx_udp_source_port = rx_udp_dest_port;
+assign tx_udp_dest_port = rx_udp_source_port;
+assign tx_udp_length = rx_udp_length;
+assign tx_udp_checksum = 0;
+
+assign tx_udp_payload_axis_tdata = tx_fifo_udp_payload_axis_tdata;
+assign tx_udp_payload_axis_tvalid = tx_fifo_udp_payload_axis_tvalid;
+assign tx_fifo_udp_payload_axis_tready = tx_udp_payload_axis_tready;
+assign tx_udp_payload_axis_tlast = tx_fifo_udp_payload_axis_tlast;
+assign tx_udp_payload_axis_tuser = tx_fifo_udp_payload_axis_tuser;
+
+assign rx_fifo_udp_payload_axis_tdata = rx_udp_payload_axis_tdata;
+assign rx_fifo_udp_payload_axis_tvalid = rx_udp_payload_axis_tvalid && match_cond_reg;
+assign rx_udp_payload_axis_tready = (rx_fifo_udp_payload_axis_tready && match_cond_reg) || no_match_reg;
+assign rx_fifo_udp_payload_axis_tlast = rx_udp_payload_axis_tlast;
+assign rx_fifo_udp_payload_axis_tuser = rx_udp_payload_axis_tuser;
+
+eth_mac_1g_rgmii_fifo #(
+    .TARGET("ALTERA"),
+    .USE_CLK90("TRUE"),
+    .ENABLE_PADDING(1),
+    .MIN_FRAME_LENGTH(64),
+    .TX_FIFO_DEPTH(4096),
+    .TX_FRAME_FIFO(1),
+    .RX_FIFO_DEPTH(4096),
+    .RX_FRAME_FIFO(1)
+)
+eth_mac_inst (
+    .gtx_clk(i_clk),
+    .gtx_clk90(i_clk90),
+    .gtx_rst(i_rst_n),
+    .logic_clk(i_clk),
+    .logic_rst(i_rst_n),
+
+    .tx_axis_tdata(tx_axis_tdata),
+    .tx_axis_tvalid(tx_axis_tvalid),
+    .tx_axis_tready(tx_axis_tready),
+    .tx_axis_tlast(tx_axis_tlast),
+    .tx_axis_tuser(tx_axis_tuser),
+
+    .rx_axis_tdata(rx_axis_tdata),
+    .rx_axis_tvalid(rx_axis_tvalid),
+    .rx_axis_tready(rx_axis_tready),
+    .rx_axis_tlast(rx_axis_tlast),
+    .rx_axis_tuser(rx_axis_tuser),
+
+    .rgmii_rx_clk(phy0_rx_clk),
+    .rgmii_rxd(phy0_rxd),
+    .rgmii_rx_ctl(phy0_rx_ctl),
+    .rgmii_tx_clk(phy0_tx_clk),
+    .rgmii_txd(phy0_txd),
+    .rgmii_tx_ctl(phy0_tx_ctl),
+    
+    .tx_fifo_overflow(),
+    .tx_fifo_bad_frame(),
+    .tx_fifo_good_frame(),
+    .rx_error_bad_frame(),
+    .rx_error_bad_fcs(),
+    .rx_fifo_overflow(),
+    .rx_fifo_bad_frame(),
+    .rx_fifo_good_frame(),
+    .speed(),
+
+    .ifg_delay(12)
+);
+
+eth_axis_rx eth_axis_rx_inst (
+    .clk(i_clk),
+    .rst(i_rst_n),
+    // AXI input
+    .s_axis_tdata(rx_axis_tdata),
+    .s_axis_tvalid(rx_axis_tvalid),
+    .s_axis_tready(rx_axis_tready),
+    .s_axis_tlast(rx_axis_tlast),
+    .s_axis_tuser(rx_axis_tuser),
+    // Ethernet frame output
+    .m_eth_hdr_valid(rx_eth_hdr_valid),
+    .m_eth_hdr_ready(rx_eth_hdr_ready),
+    .m_eth_dest_mac(rx_eth_dest_mac),
+    .m_eth_src_mac(rx_eth_src_mac),
+    .m_eth_type(rx_eth_type),
+    .m_eth_payload_axis_tdata(rx_eth_payload_axis_tdata),
+    .m_eth_payload_axis_tvalid(rx_eth_payload_axis_tvalid),
+    .m_eth_payload_axis_tready(rx_eth_payload_axis_tready),
+    .m_eth_payload_axis_tlast(rx_eth_payload_axis_tlast),
+    .m_eth_payload_axis_tuser(rx_eth_payload_axis_tuser),
+    // Status signals
+    .busy(),
+    .error_header_early_termination()
+);
+
+eth_axis_tx
+eth_axis_tx_inst (
+    .clk(i_clk),
+    .rst(i_rst_n),
+    // Ethernet frame input
+    .s_eth_hdr_valid(tx_eth_hdr_valid),
+    .s_eth_hdr_ready(tx_eth_hdr_ready),
+    .s_eth_dest_mac(tx_eth_dest_mac),
+    .s_eth_src_mac(tx_eth_src_mac),
+    .s_eth_type(tx_eth_type),
+    .s_eth_payload_axis_tdata(tx_eth_payload_axis_tdata),
+    .s_eth_payload_axis_tvalid(tx_eth_payload_axis_tvalid),
+    .s_eth_payload_axis_tready(tx_eth_payload_axis_tready),
+    .s_eth_payload_axis_tlast(tx_eth_payload_axis_tlast),
+    .s_eth_payload_axis_tuser(tx_eth_payload_axis_tuser),
+    // AXI output
+    .m_axis_tdata(tx_axis_tdata),
+    .m_axis_tvalid(tx_axis_tvalid),
+    .m_axis_tready(tx_axis_tready),
+    .m_axis_tlast(tx_axis_tlast),
+    .m_axis_tuser(tx_axis_tuser),
+    // Status signals
+    .busy()
+);
+
+udp_complete
+udp_complete_inst (
+    .clk(i_clk),
+    .rst(i_rst_n),
+    // Ethernet frame input
+    .s_eth_hdr_valid(rx_eth_hdr_valid),
+    .s_eth_hdr_ready(rx_eth_hdr_ready),
+    .s_eth_dest_mac(rx_eth_dest_mac),
+    .s_eth_src_mac(rx_eth_src_mac),
+    .s_eth_type(rx_eth_type),
+    .s_eth_payload_axis_tdata(rx_eth_payload_axis_tdata),
+    .s_eth_payload_axis_tvalid(rx_eth_payload_axis_tvalid),
+    .s_eth_payload_axis_tready(rx_eth_payload_axis_tready),
+    .s_eth_payload_axis_tlast(rx_eth_payload_axis_tlast),
+    .s_eth_payload_axis_tuser(rx_eth_payload_axis_tuser),
+    // Ethernet frame output
+    .m_eth_hdr_valid(tx_eth_hdr_valid),
+    .m_eth_hdr_ready(tx_eth_hdr_ready),
+    .m_eth_dest_mac(tx_eth_dest_mac),
+    .m_eth_src_mac(tx_eth_src_mac),
+    .m_eth_type(tx_eth_type),
+    .m_eth_payload_axis_tdata(tx_eth_payload_axis_tdata),
+    .m_eth_payload_axis_tvalid(tx_eth_payload_axis_tvalid),
+    .m_eth_payload_axis_tready(tx_eth_payload_axis_tready),
+    .m_eth_payload_axis_tlast(tx_eth_payload_axis_tlast),
+    .m_eth_payload_axis_tuser(tx_eth_payload_axis_tuser),
+    // IP frame input
+    .s_ip_hdr_valid(tx_ip_hdr_valid),
+    .s_ip_hdr_ready(tx_ip_hdr_ready),
+    .s_ip_dscp(tx_ip_dscp),
+    .s_ip_ecn(tx_ip_ecn),
+    .s_ip_length(tx_ip_length),
+    .s_ip_ttl(tx_ip_ttl),
+    .s_ip_protocol(tx_ip_protocol),
+    .s_ip_source_ip(tx_ip_source_ip),
+    .s_ip_dest_ip(tx_ip_dest_ip),
+    .s_ip_payload_axis_tdata(tx_ip_payload_axis_tdata),
+    .s_ip_payload_axis_tvalid(tx_ip_payload_axis_tvalid),
+    .s_ip_payload_axis_tready(tx_ip_payload_axis_tready),
+    .s_ip_payload_axis_tlast(tx_ip_payload_axis_tlast),
+    .s_ip_payload_axis_tuser(tx_ip_payload_axis_tuser),
+    // IP frame output
+    .m_ip_hdr_valid(rx_ip_hdr_valid),
+    .m_ip_hdr_ready(rx_ip_hdr_ready),
+    .m_ip_eth_dest_mac(rx_ip_eth_dest_mac),
+    .m_ip_eth_src_mac(rx_ip_eth_src_mac),
+    .m_ip_eth_type(rx_ip_eth_type),
+    .m_ip_version(rx_ip_version),
+    .m_ip_ihl(rx_ip_ihl),
+    .m_ip_dscp(rx_ip_dscp),
+    .m_ip_ecn(rx_ip_ecn),
+    .m_ip_length(rx_ip_length),
+    .m_ip_identification(rx_ip_identification),
+    .m_ip_flags(rx_ip_flags),
+    .m_ip_fragment_offset(rx_ip_fragment_offset),
+    .m_ip_ttl(rx_ip_ttl),
+    .m_ip_protocol(rx_ip_protocol),
+    .m_ip_header_checksum(rx_ip_header_checksum),
+    .m_ip_source_ip(rx_ip_source_ip),
+    .m_ip_dest_ip(rx_ip_dest_ip),
+    .m_ip_payload_axis_tdata(rx_ip_payload_axis_tdata),
+    .m_ip_payload_axis_tvalid(rx_ip_payload_axis_tvalid),
+    .m_ip_payload_axis_tready(rx_ip_payload_axis_tready),
+    .m_ip_payload_axis_tlast(rx_ip_payload_axis_tlast),
+    .m_ip_payload_axis_tuser(rx_ip_payload_axis_tuser),
+    // UDP frame input
+    .s_udp_hdr_valid(tx_udp_hdr_valid),
+    .s_udp_hdr_ready(tx_udp_hdr_ready),
+    .s_udp_ip_dscp(tx_udp_ip_dscp),
+    .s_udp_ip_ecn(tx_udp_ip_ecn),
+    .s_udp_ip_ttl(tx_udp_ip_ttl),
+    .s_udp_ip_source_ip(tx_udp_ip_source_ip),
+    .s_udp_ip_dest_ip(tx_udp_ip_dest_ip),
+    .s_udp_source_port(tx_udp_source_port),
+    .s_udp_dest_port(tx_udp_dest_port),
+    .s_udp_length(tx_udp_length),
+    .s_udp_checksum(tx_udp_checksum),
+    .s_udp_payload_axis_tdata(tx_udp_payload_axis_tdata),
+    .s_udp_payload_axis_tvalid(tx_udp_payload_axis_tvalid),
+    .s_udp_payload_axis_tready(tx_udp_payload_axis_tready),
+    .s_udp_payload_axis_tlast(tx_udp_payload_axis_tlast),
+    .s_udp_payload_axis_tuser(tx_udp_payload_axis_tuser),
+    // UDP frame output
+    .m_udp_hdr_valid(rx_udp_hdr_valid),
+    .m_udp_hdr_ready(rx_udp_hdr_ready),
+    .m_udp_eth_dest_mac(rx_udp_eth_dest_mac),
+    .m_udp_eth_src_mac(rx_udp_eth_src_mac),
+    .m_udp_eth_type(rx_udp_eth_type),
+    .m_udp_ip_version(rx_udp_ip_version),
+    .m_udp_ip_ihl(rx_udp_ip_ihl),
+    .m_udp_ip_dscp(rx_udp_ip_dscp),
+    .m_udp_ip_ecn(rx_udp_ip_ecn),
+    .m_udp_ip_length(rx_udp_ip_length),
+    .m_udp_ip_identification(rx_udp_ip_identification),
+    .m_udp_ip_flags(rx_udp_ip_flags),
+    .m_udp_ip_fragment_offset(rx_udp_ip_fragment_offset),
+    .m_udp_ip_ttl(rx_udp_ip_ttl),
+    .m_udp_ip_protocol(rx_udp_ip_protocol),
+    .m_udp_ip_header_checksum(rx_udp_ip_header_checksum),
+    .m_udp_ip_source_ip(rx_udp_ip_source_ip),
+    .m_udp_ip_dest_ip(rx_udp_ip_dest_ip),
+    .m_udp_source_port(rx_udp_source_port),
+    .m_udp_dest_port(rx_udp_dest_port),
+    .m_udp_length(rx_udp_length),
+    .m_udp_checksum(rx_udp_checksum),
+    .m_udp_payload_axis_tdata(rx_udp_payload_axis_tdata),
+    .m_udp_payload_axis_tvalid(rx_udp_payload_axis_tvalid),
+    .m_udp_payload_axis_tready(rx_udp_payload_axis_tready),
+    .m_udp_payload_axis_tlast(rx_udp_payload_axis_tlast),
+    .m_udp_payload_axis_tuser(rx_udp_payload_axis_tuser),
+    // Status signals
+    .ip_rx_busy(),
+    .ip_tx_busy(),
+    .udp_rx_busy(),
+    .udp_tx_busy(),
+    .ip_rx_error_header_early_termination(),
+    .ip_rx_error_payload_early_termination(),
+    .ip_rx_error_invalid_header(),
+    .ip_rx_error_invalid_checksum(),
+    .ip_tx_error_payload_early_termination(),
+    .ip_tx_error_arp_failed(),
+    .udp_rx_error_header_early_termination(),
+    .udp_rx_error_payload_early_termination(),
+    .udp_tx_error_payload_early_termination(),
+    // Configuration
+    .local_mac(local_mac),
+    .local_ip(local_ip),
+    .gateway_ip(gateway_ip),
+    .subnet_mask(subnet_mask),
+    .clear_arp_cache(0)
+);
+
+axis_fifo #(
+    .DEPTH(8192),
+    .DATA_WIDTH(8),
+    .KEEP_ENABLE(0),
+    .ID_ENABLE(0),
+    .DEST_ENABLE(0),
+    .USER_ENABLE(1),
+    .USER_WIDTH(1),
+    .FRAME_FIFO(0)
+)
+udp_payload_fifo (
+    .clk(i_clk),
+    .rst(i_rst_n),
+
+    // AXI input
+    .s_axis_tdata(rx_fifo_udp_payload_axis_tdata),
+    .s_axis_tkeep(0),
+    .s_axis_tvalid(rx_fifo_udp_payload_axis_tvalid),
+    .s_axis_tready(rx_fifo_udp_payload_axis_tready),
+    .s_axis_tlast(rx_fifo_udp_payload_axis_tlast),
+    .s_axis_tid(0),
+    .s_axis_tdest(0),
+    .s_axis_tuser(rx_fifo_udp_payload_axis_tuser),
+
+    // AXI output
+    .m_axis_tdata(tx_fifo_udp_payload_axis_tdata),
+    .m_axis_tkeep(),
+    .m_axis_tvalid(tx_fifo_udp_payload_axis_tvalid),
+    .m_axis_tready(tx_fifo_udp_payload_axis_tready),
+    .m_axis_tlast(tx_fifo_udp_payload_axis_tlast),
+    .m_axis_tid(),
+    .m_axis_tdest(),
+    .m_axis_tuser(tx_fifo_udp_payload_axis_tuser),
+
+    // Status
+    .status_overflow(),
+    .status_bad_frame(),
+    .status_good_frame()
+);
+    
+endmodule
